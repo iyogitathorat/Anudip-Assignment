b3fba2c6f7017fccbe3041857dea97fe2b08c6e4
Yogita Satish Thorat <95094214+iyogitathorat@users.noreply.github.com>
2021-12-11 19:42:32 +0530

    Add files via upload

---
+++ Addition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 6;
+            int sum = x + y;
+            Console.WriteLine(sum);
+        }
+    }
+}
