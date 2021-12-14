41834bc09a8d81294c036555bf21b0cb53c439e5
Yogita Satish Thorat <95094214+iyogitathorat@users.noreply.github.com>
2021-12-14 13:39:40 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,12 +6,32 @@ namespace Arrays
     {
         public static void Main(string[] args)
         {
-            int[] arr = new int[] { 10, 11, 100, 101, 110 };
-            for (int i = 0; i < arr.Length; i++)
+            int i, j;
+            int[,] arr = new int[2, 2];
+
+            Console.WriteLine("Enter the elements for 2 X 2 matrix");
+
+            for (i = 0; i < 2; i++)
             {
-                Console.WriteLine(arr[i]);
-                Console.ReadLine();
+                for (j = 0; j < 2; j++)
+                {
+                    Console.Write("Enter element at [{0},{1}] = ", i, j);
+
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (i = 0; i < 2; i++)
+            {
+                Console.Write("\n");
+
+                for (j = 0; j < 2; j++)
+                {
+                Console.Write("{0}\t", arr[i, j]);
+                }
             }
         }
     }
 }
+
+
+
