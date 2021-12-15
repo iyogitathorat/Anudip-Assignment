ee77577aed7d5f17931681a88d31f0d8eedbb166
Yogita Satish Thorat <95094214+iyogitathorat@users.noreply.github.com>
2021-12-15 16:43:40 +0530

    Add files via upload

---
+++ String Type.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace string_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] arr = new string[] { "c++", "java", "c#" };
+            foreach (String a in arr)
+            {
+                Console.WriteLine(a);
+            }
+            Console.ReadLine();
+        }
+    }
+}
