d5da842f903dd04c5128c9906fe0f1800700e398
Yogita Satish Thorat <95094214+iyogitathorat@users.noreply.github.com>
2021-12-17 11:44:42 +0530

    Add files via upload

---
+++ dyanamic data.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+class Demo
+{
+    dynamic Data = 12;
+    public int Method(int X, int Y)
+    {
+        return (X + Y) * Data;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Demo Obj = new Demo();
+        dynamic value1 = 5;
+        dynamic value2 = 6;
+        dynamic Str = "Your Result Is: ";
+        Console.WriteLine(Str + Obj.Method(value1, value2));
+        Console.ReadLine();
+    }
+}
+
