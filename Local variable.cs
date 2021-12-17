d7cd09475188651aab2163d61b3375e995e03b01
Yogita Satish Thorat <95094214+iyogitathorat@users.noreply.github.com>
2021-12-17 11:34:42 +0530

    Add files via upload

---
+++ Local variable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace local_variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //float
+            var a = 2.755;
+            //string
+            var s = "javapoint";
+            //array
+            var arr = new[] { 1, 2, 3 };
+            Console.WriteLine(a);
+            Console.WriteLine(s);
+            Console.WriteLine(arr[1]);
+        }
+    }
+}
+       
