8571444d9ad4557679f1e3c92e2bc2b64fe25612
Yogita Satish Thorat <95094214+iyogitathorat@users.noreply.github.com>
2021-12-31 12:14:11 +0530

    Add files via upload

---
+++ Inheritance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+
+    using System;
+    public class Employee
+    {
+        public float salary = 40000;
+    }
+    public class Programmer : Employee
+    {
+        public float bonus = 10000;
+    }
+    class TestInheritance
+    {
+        public static void Main(string[] args)
+        {
+            Programmer p1 = new Programmer();
+
+            Console.WriteLine("Salary: " + p1.salary);
+            Console.WriteLine("Bonus: " + p1.bonus);
+
+        }
+    }
+}
