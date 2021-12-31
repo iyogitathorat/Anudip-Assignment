d77460fb3dd2c4d864d3c6fa3671606885660f84
Yogita Satish Thorat <95094214+iyogitathorat@users.noreply.github.com>
2021-12-31 11:56:31 +0530

    Add files via upload

---
+++ As operator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Demo
+{
+    public static void Main()
+    {
+        object[] obj = new object[2];
+        obj[0] = "Yogita";
+        obj[1] = 20;
+        for (int i = 0; i < obj.Length; ++i)
+        {
+            string s = obj[i] as string;
+            Console.Write("{0}: ", i);
+            if (s != null)
+                Console.WriteLine("'" + s + "'");
+            else
+                Console.WriteLine("This is not a string!");
+        }
+        Console.ReadKey();
+    }
+}
+
