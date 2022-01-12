13f3e82ba3ad0dd968eeba4420bc368515106c5b
Yogita Satish Thorat <95094214+iyogitathorat@users.noreply.github.com>
2022-01-11 20:37:29 -0800

    Add files via upload

---
+++ Regular Expression2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace RegularExpression2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hi,welcome@DotNet.com";
+
+            string result = Regex.Replace(str, "[^a-zA-Z0-9_]+", " ");
+            Console.WriteLine("{0}", result);
+            Console.ReadLine();
+        }
+    }
+}
