43a63566510aab63065ac6ae33f15ce253a5e033
Yogita Satish Thorat <95094214+iyogitathorat@users.noreply.github.com>
2022-01-11 20:34:37 -0800

    Add files via upload

---
+++ Regular Expression 1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace RegularExpression2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string email = "yogitathorat23@gmail.com";
+            var result = Regex.IsMatch(email, @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+            Console.WriteLine("is valid:{0}", result);
+            Console.ReadLine();
+        }
+    }
+}
